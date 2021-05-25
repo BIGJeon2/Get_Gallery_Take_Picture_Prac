d863b5207d78e57622854ee47212ff7c535b090a
Big_Jeon <robert6728@naver.com>
2021-05-25 14:49:29 +0900

    Add_Permisiionm_Check_With_TedPermission

--- app/src/main/java/com/example/take_picure_by_camera_practice/MainActivity.kt
+++ app/src/main/java/com/example/take_picure_by_camera_practice/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.take_picure_by_camera_practice.databinding.ActivityMainBinding
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.squareup.picasso.Picasso
+import java.security.Permission
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_CODE_CAMERA_PERMISSION = 1001
+    private val REQUEST_CODE_CAMERA_PERMISSION = 1
     private val get_img_requestCode = 1001
     private val Perimisiions = arrayListOf<String>("android.perimisiion.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE")
     private var img_Uri: Uri? = null
@@ -18,6 +22,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
         setContentView(view)
+
+        setPermission()
+
         //사진가져오기
         binding.GetPictureByGallery.setOnClickListener {
             val get_image = Intent(Intent.ACTION_PICK)
@@ -30,17 +37,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setPermission() {
+        val permisiion = object : PermissionListener{
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@MainActivity, "권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPermissionGranted() {
+                Toast.makeText(this@MainActivity, "권한이 설정되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        TedPermission.with(this)
+            .setPermissionListener(permisiion)
+            .setRationaleMessage("카메라 사용을 위해 권한을 허용해 주세요")
+            .setDeniedMessage("거부되셨습니다.")
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            .check()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == get_img_requestCode && resultCode == RESULT_OK){
             img_Uri = data?.data
             Picasso.get().load(img_Uri).into(binding.ImgView)
         }
-    }
-
-    //사진 이미지뷰에 출력
-    private fun Set_Picture(){
-
     }
     private fun Cheack_Permisiion(){
 
